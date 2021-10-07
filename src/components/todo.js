59af372135edf81a9f7c12d6a4f16ed130424c3a
Sanketjn18 <sanketjain070@gmail.com>
2021-10-07 20:11:19 +0530

    add styles

--- src/components/todo.js
+++ src/components/todo.js
@@ -9,6 +9,7 @@ import {
   addTodo,
   toggleTodoById,
 } from "../redux/todo";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Todo() {
   const { allTodo, selectedTodo } = useSelector((state) => state.todoList);
@@ -20,22 +21,22 @@ export default function Todo() {
   return (
     <React.Fragment>
       <div className="addTodo">
-        <label>
-          Add New :{" "}
-          <input
-            className="inputTodo"
-            onChange={(e) => setItem(e.target.value)}
-            value={item}
-          />
-        </label>
+        <label htmlFor="todoInputBox">Add New : </label>
+        <input
+          id="todoInputBox"
+          className="inputTodo"
+          onChange={(e) => setItem(e.target.value)}
+          value={item}
+        />
+
         <button
-          className="addButton"
+          className="actionButton"
           onClick={() => {
             if (item.length > 0) {
               dispatch(
                 addTodo({
                   UserId: 1,
-                  id: allTodo.length + 1,
+                  id: uuidv4(),
                   title: item,
                   completed: false,
                 })
@@ -44,43 +45,53 @@ export default function Todo() {
             }
           }}
         >
-          Add
+          Create
         </button>
       </div>
 
       <div className="innerDiv">
         <div className="todoList">
-            <h5>All Todos</h5>
-          <ol>
-            {(allTodo &&
-              allTodo.length > 0) ?
-              allTodo.flat().map((item, i) => {
-                console.log(item.id, "id");
-                return (
-                  <li
-                    onClick={() => dispatch(selectTodoById(item.id))}
-                    key={item.id}
-                    className={item.completed ? "completed" : ""}
-                  >
-                    {item.title}{" "}
-                  </li>
-                );
-              })
-            :
-            "loading"}
-          </ol>
+          <h2>All Todos</h2>
+          <div className="todoListItem">
+            {allTodo && allTodo.length > 0
+              ? allTodo.flat().map((item, i) => {
+                  return (
+                    <p
+                      onClick={() => dispatch(selectTodoById(item.id))}
+                      key={item.id}
+                      className={item.completed ? "completed" : ""}
+                    >
+                      {i + 1}
+                      {". "}
+                      {item.title}{" "}
+                    </p>
+                  );
+                })
+              : "loading"}
+          </div>
         </div>
         <div className="selectedTodo">
-            <h5>Selected Todo</h5>
-          <p>
-            {selectedTodo && selectedTodo.length > 0
-              ? selectedTodo[0].title
-              : "No todo selected"}
-          </p>
-          <button onClick={() => { dispatch(deleteTodoById(selectedTodo[0].id)); setItem("") }}>
+          <h2>Selected Todo</h2>
+          {selectedTodo && selectedTodo.length > 0 ? (
+            <h4 className={selectedTodo[0].completed ? "completed" : ""}>
+              {selectedTodo[0].title}
+            </h4>
+          ) : (
+            "No todo selected"
+          )}
+          <button
+            className="actionButton selected"
+            onClick={() => {
+              dispatch(deleteTodoById(selectedTodo[0].id));
+              setItem("");
+            }}
+          >
             Delete
           </button>
-          <button onClick={() => dispatch(toggleTodoById(selectedTodo[0].id))}>
+          <button
+            className="actionButton selected"
+            onClick={() => dispatch(toggleTodoById(selectedTodo[0].id))}
+          >
             Toggle
           </button>
         </div>
